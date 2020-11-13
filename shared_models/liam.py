cbbd05e2d62dac7c6a05237a62dd5e7c706caa35
hanandito <hananditos@gmail.com>
2020-11-13 18:33:04 +0700

    liam update

--- shared_models/liam.py
+++ shared_models/liam.py
@@ -10,40 +10,46 @@ class LiamFelicia(models.Model):
     u_id = models.ForeignKey(User, on_delete=models.CASCADE)
     theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
     site_url = models.SlugField(unique=True,
-                                default='Your wedding invitation url. e.g. reservasidulu.com/bride-groom')
+                                default='Your wedding invitation url. e.g. reservasidulu.com/passionate-bond')
 
     # section 1
-    title = models.CharField(max_length=40, help_text='Your wedding title')
-    bride_name = models.CharField(max_length=30, default='Your bride name')
-    grome_name = models.CharField(max_length=30, default='Your groom name')
-    hashtag = models.CharField(max_length=30, default='Your #hashtag here')
-    img_logo = models.ImageField(upload_to='img/liam/', blank=False, help_text='Your logo goes here')
-
+        title = models.CharField(max_length=40, help_text='Your wedding title')
+        initial_logo = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your logo image')
+        groom_name = models.CharField(max_length=30, default='Your groom name')
+        bride_name = models.CharField(max_length=30, default='Your bride name')
+        hashtag = models.CharField(max_length=30, default='Your #hashtag here')
     # section 2
-    banner_bg = models.ImageField(upload_to='img/liam/', blank=True)
-    banner_title = models.CharField(max_length=50, default='')
-    section2_image = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your summary image')
-    about_title = models.CharField(blank=False, max_length=200, help_text='Your summary brief title')
-    about_brief = models.TextField(blank=False, help_text='Your summary brief')
-
+        banner_title = models.CharField(max_length=40, default='Your title')
+        banner_bg = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your banner image')
+        about_image = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='About image')
+        about_title = models.CharField(max_length=40, default='Your title')
+        about_brief = models.TextField(blank=False, default='Your summary brief')
     # section 3
-    ceremony_date = models.CharField(max_length=100, default='Saturday, 22th July 2020')
-    ceremony_time = models.CharField(max_length=100, default='3:00pm – 4:00pm')
-    ceremony_place = models.TextField(default='New Faith Church '
-                                              '415 Greenwich St (at Bethune St) New York, NY 10014 (212) 555–0123')
-
-    reception_date = models.CharField(max_length=100, default='Saturday, 22th July 2020')
-    reception_time = models.CharField(max_length=100, default='3:00pm – 4:00pm')
-    reception_place = models.TextField(default='New Faith Church'
-                                              '415 Greenwich St (at Bethune St) New York, NY 10014 (212) 555–0123')
-
+        vc_image = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your image')
+        vc_day = models.CharField(max_length=40, default='Saturday')
+        vc_date = models.CharField(max_length=40, default='22th')
+        vc_time = models.CharField(max_length=40, default='3:00pm – 4:00pm')
+        vc_place = models.CharField(max_length=40, default='New Faith Church')
+        vc_address = models.TextField(blank=False, default='Address detail')
+        vr_image = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your image')
+        vr_day = models.CharField(max_length=40, default='Saturday')
+        vr_date = models.CharField(max_length=40, default='22th')
+        vr_time = models.CharField(max_length=40, default='3:00pm – 4:00pm')
+        vr_place = models.CharField(max_length=40, default='New Faith Church')
+        vr_address = models.TextField(blank=False, default='Address detail')
     # section 4
-    gallery1 = models.ImageField(blank=True, upload_to='img/liam/', help_text='Your gallery')
-    gallery2 = models.ImageField(blank=True, upload_to='img/liam/', help_text='Your gallery')
-    gallery3 = models.ImageField(blank=True, upload_to='img/liam/', help_text='Your gallery')
-    gallery4 = models.ImageField(blank=True, upload_to='img/liam/', help_text='Your gallery')
-
-    rsvp_img = models.ImageField(upload_to='img/liam/', null=True)
+        acm_image = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your image')
+        acm_place = models.CharField(max_length=40, default='Place')
+        acm_address = models.TextField(blank=False, default='Address Detail')
+        acm_note = models.TextField(blank=False, default='Note Detail')
+    # section 5
+        gallery_image1 = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your image')
+        gallery_image2 = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your image')
+        gallery_image3 = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your image')
+        gallery_image4 = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your image')
+        gallery_image5 = models.ImageField(upload_to='img/liam/', blank=True, null=True, help_text='Your image')
+    # section 6
+        inv_pdf = models.FileField(upload_to='file/liam/', blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True, auto_created=True, editable=False)
     updated = models.DateTimeField(auto_created=True, auto_now=True, editable=True)
